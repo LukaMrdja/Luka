fbddfc11ce91215ebaf296bc9003e6925b8a118d
LukaMrdja <luka.mrda@yahoo.com>
2023-10-27 12:50:14 +0200

    update

---
+++ Luka-main/EdunovaApp/Controllers/PacijentController.cs
@@ -0,0 +1,252 @@
+﻿using EdunovaApp.Data;
+using EdunovaApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace EdunovaApp.Controllers
+{
+    /// <summary>
+    /// Namijenjeno za CRUD operacije na entitetom smjer u bazi
+    /// </summary>
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    
+    public class PacijentController : ControllerBase
+    {
+
+        // Dependency injection u controller
+        // https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/adding-model?view=aspnetcore-7.0&tabs=visual-studio#dependency-injection
+        private readonly EdunovaContext _context;
+        private readonly ILogger<PacijentController> _logger;
+
+        public PacijentController(EdunovaContext context, ILogger<PacijentController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Dohvaća sve smjerove iz baze
+        /// </summary>
+        /// <remarks>
+        /// Primjer upita:
+        ///
+        ///    GET api/v1/Smjer
+        ///
+        /// </remarks>
+        /// <returns>Smjerovi u bazi</returns>
+        /// <response code="200">Sve je u redu</response>
+        /// <response code="400">Zahtjev nije valjan (BadRequest)</response> 
+        /// <response code="503">Na azure treba dodati IP u firewall</response> 
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // kontrola ukoliko upit nije dobar
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var Pacijent = _context.Pacijent.ToList();
+                if(Pacijent == null || Pacijent.Count == 0)
+                {
+                    return new EmptyResult();
+                }
+                return new JsonResult(_context.Pacijent.ToList());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status503ServiceUnavailable, 
+                                    ex.Message);
+            }
+
+            
+            
+        }
+
+
+        [HttpGet]
+        [Route("{sifra:int}")]
+        public IActionResult GetBySifra(int sifra) { 
+        
+            if (sifra <= 0)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try {
+                var s = _context.Pacijent.Find(sifra);
+
+                if (s == null)
+                {
+                    return StatusCode(StatusCodes.Status204NoContent, s);
+                }
+
+                return new JsonResult(s);
+
+            } catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status503ServiceUnavailable, ex.Message);
+            }
+
+        }
+
+
+        /// <summary>
+        /// Dodaje smjer u bazu
+        /// </summary>
+        /// <remarks>
+        /// Primjer upita:
+        ///
+        ///    POST api/v1/Smjer
+        ///    {naziv:"",trajanje:100}
+        ///
+        /// </remarks>
+        /// <returns>Kreirani smjer u bazi s svim podacima</returns>
+        /// <response code="200">Sve je u redu</response>
+        /// <response code="400">Zahtjev nije valjan (BadRequest)</response> 
+        /// <response code="503">Na azure treba dodati IP u firewall</response> 
+        [HttpPost]
+        public IActionResult Post(Pacijent Pacijent)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                _context.Pacijent.Add(Pacijent);
+                _context.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created, Pacijent);
+            }catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status503ServiceUnavailable,
+                                   ex.Message);
+            }
+            
+
+            
+        }
+
+
+
+
+        /// <summary>
+        /// Mijenja podatke postojećeg smjera u bazi
+        /// </summary>
+        /// <remarks>
+        /// Primjer upita:
+        ///
+        ///    PUT api/v1/smjer/1
+        ///
+        /// {
+        ///  "sifra": 0,
+        ///  "naziv": "Novi naziv",
+        ///  "trajanje": 120,
+        ///  "cijena": 890.22,
+        ///  "upisnina": 0,
+        ///  "verificiran": true
+        /// }
+        ///
+        /// </remarks>
+        /// <param name="sifra">Šifra smjera koji se mijenja</param>
+        /// <param name="Pacijent"></param>  
+        /// <returns>Svi poslani podaci od smjera</returns>
+        /// <response code="200">Sve je u redu</response>
+        /// <response code="204">Nema u bazi smjera kojeg želimo promijeniti</response>
+        /// <response code="415">Nismo poslali JSON</response> 
+        /// <response code="503">Na azure treba dodati IP u firewall</response> 
+        [HttpPut]
+        [Route("{sifra:int}")]
+        public IActionResult Put(int sifra, Pacijent Pacijent) 
+            {
+        
+            if (sifra<=0 || Pacijent==null)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                var smjerBaza = _context.Pacijent.Find(sifra);
+                if (smjerBaza == null)
+                {
+                    return BadRequest();
+                }
+                // inače se rade Mapper-i
+                // mi ćemo za sada ručno
+                smjerBaza.Ime = Pacijent.Ime;
+                smjerBaza.Prezime = Pacijent.Prezime;
+                smjerBaza.telefon = Pacijent.telefon;
+                smjerBaza.BrojZk = Pacijent.BrojZk;             
+
+                _context.Pacijent.Update(smjerBaza);
+                _context.SaveChanges();
+
+                return StatusCode(StatusCodes.Status200OK, smjerBaza);
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status503ServiceUnavailable,
+                                  ex); // kada se vrati cijela instanca ex tada na klijentu imamo više podataka o grešci
+                // nije dobro vraćati cijeli ex ali za dev je OK
+            }
+           
+        }
+
+
+        /// <summary>
+        /// Briše smjer iz baze
+        /// </summary>
+        /// <remarks>
+        /// Primjer upita:
+        ///
+        ///    DELETE api/v1/smjer/1
+        ///    
+        /// </remarks>
+        /// <param name="sifra">Šifra smjera koji se briše</param>  
+        /// <returns>Odgovor da li je obrisano ili ne</returns>
+        /// <response code="200">Sve je u redu</response>
+        /// <response code="204">Nema u bazi smjera kojeg želimo obrisati</response>
+        /// <response code="415">Nismo poslali JSON</response> 
+        /// <response code="503">Na azure treba dodati IP u firewall</response> 
+        [HttpDelete]
+        [Route("{sifra:int}")]
+        [Produces("application/json")]
+        public IActionResult Delete(int sifra)
+        {
+            if (sifra <= 0)
+            {
+                return BadRequest();
+            }
+
+            var smjerBaza = _context.Pacijent.Find(sifra);
+            if (smjerBaza == null)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                _context.Pacijent.Remove(smjerBaza);
+                _context.SaveChanges();
+
+                return new JsonResult("{\"poruka\":\"Obrisano\"}");
+
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status400BadRequest,
+                                  "Ne može se obrisati smjer jer ima na sebi grupe");
+
+               // new JsonResult("{\"poruka\":\"Ne može se obrisati\"}");
+
+            }
+        }
+    }
+}
